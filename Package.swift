6a2025cb6551383f7ff83bc01e5fea4bc154cea3
Roman Mazeev <romanmazeev@icloud.com>
2021-07-01 17:33:25 +0300

    Update dependency version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/appintheair/MRZParser.git", .upToNextMajor(from: "1.0.0"))
+        .package(url: "https://github.com/appintheair/MRZParser.git", .upToNextMajor(from: "1.0.1"))
     ],
     targets: [
         .target(

7e3f3da87b0d5859c875c924e6f3f793f73f5d11
Roman Mazeev <romanmazeev@icloud.com>
2021-07-12 02:09:46 +0300

    Refactoring
    Make easier to use the library

---
+++ Sources/MRZScanner/LiveResultTracker.swift
@@ -0,0 +1,46 @@
+//
+//  LiveResultTracker.swift
+//
+//
+//  Created by Roman Mazeev on 16.06.2021.
+//
+
+import Foundation
+import MRZParser
+
+/// Needed for scanning documents in live mode. Allows you to control the accuracy of results when using multiple frames
+public class LiveResultTracker {
+    /// Scan result and its accuracy based on frequency of occurrence
+    typealias ScanningResult = (mrzResult: MRZResult, accuracy: Int)
+    private typealias ResultObservation = (lastSeen: Int, count: Int)
+
+    /// Dictionary of seen results. Used to get stable recognition before displaying anything.
+    private var seenMRZResults: [MRZResult: ResultObservation] = [:]
+    private var frameIndex = 0
+
+    var liveScanningResult: ScanningResult? {
+        guard let mostProbableResult = seenMRZResults.sorted(by: {
+            $0.value.count > $1.value.count
+        }).first else { return nil }
+        return (mostProbableResult.key, mostProbableResult.value.count)
+    }
+
+    func track(result: MRZResult) {
+        // Remove old results (~1s)
+        seenMRZResults = seenMRZResults.filter { $0.value.lastSeen < frameIndex - 30 }
+
+        if seenMRZResults[result] == nil {
+            seenMRZResults[result] = (lastSeen: 0, count: -1)
+        }
+
+        seenMRZResults[result]?.lastSeen = frameIndex
+        seenMRZResults[result]?.count += 1
+
+        frameIndex += 1
+    }
+
+    func reset() {
+        seenMRZResults = [:]
+        frameIndex = 0
+    }
+}
--- Sources/MRZScanner/MRZScanner.swift
+++ Sources/MRZScanner/MRZScanner.swift
@@ -8,8 +8,13 @@
 import Vision
 import MRZParser
 
+/// Scan result and its accuracy based on frequency of occurrence
+public typealias LiveScanningResult = (mrzResult: MRZResult, accuracy: Int)
+
 public protocol MRZScannerDelegate: AnyObject {
+    /// Passes the result of the scan
     func mrzScanner(_ scanner: MRZScanner, didReceiveResult result: ScanningResult)
+    /// Passes the bounding rects of mrz strings
     func mrzScanner(_ scanner: MRZScanner, didFindBoundingRects rects: [CGRect])
 }
 
@@ -19,8 +24,10 @@ public extension MRZScannerDelegate {
 
 /// Result of scanning
 public enum ScanningResult {
-    /// Successful scan result. `Accuracy` is the number of successfully recognized frames with this result
-    case success(mrzResult: MRZResult, accuracy: Int)
+    /// Successful scan result using live capture. `Accuracy` is the number of successfully recognized frames with this result
+    case liveSuccess(LiveScanningResult)
+    /// Successful scan when `!isLive`.
+    case imageSuccess(MRZResult: MRZResult)
     /// No MRZ code was detected on the scanned image
     case noValidMRZ
     /// An error occurred during the request execution
@@ -28,9 +35,10 @@ public enum ScanningResult {
 }
 
 public class MRZScanner {
-    private let parser = MRZParser()
-    public let tracker = ResultTracker()
     public weak var delegate: MRZScannerDelegate?
+
+    private let parser = MRZParser()
+    private let liveResultTracker = LiveResultTracker()
 
     public init() {}
 
@@ -40,17 +48,17 @@ public class MRZScanner {
     ///   - pixelBuffer: Image.
     ///   - orientation: Image orientation
     ///   - regionOfInterest: Only run on the region of interest for maximum speed.
+    ///   - isLive: Needed to set the `recognitionLevel` of the Vision request and  for `LiveResultTracker` using
     ///   - minimumTextHeight: The minimum height of the text expected to be recognized, relative to the image height
-    ///   - recognitionLevel: For live recording it is better to use `.fast` otherwise `.accurate`
     public func scan(pixelBuffer: CVPixelBuffer,
                      orientation: CGImagePropertyOrientation,
                      regionOfInterest: CGRect,
-                     minimumTextHeight: Float = 0.1,
-                     recognitionLevel: VNRequestTextRecognitionLevel = .fast) {
-        let request = createRequest()
+                     isLive: Bool = false,
+                     minimumTextHeight: Float = 0.1) {
+        let request = createRequest(isLive: isLive)
         request.regionOfInterest = regionOfInterest
         request.minimumTextHeight = minimumTextHeight
-        request.recognitionLevel = recognitionLevel
+        request.recognitionLevel = isLive ? .fast : .accurate
         request.usesLanguageCorrection = false
 
         let imageRequestHandler = VNImageRequestHandler(cvPixelBuffer: pixelBuffer,
@@ -66,12 +74,23 @@ public class MRZScanner {
         }
     }
 
-    private func createRequest() -> VNRecognizeTextRequest {
+    /// Resets `LiveResultTracker` state
+    public func resetLiveScanningSession() {
+        liveResultTracker.reset()
+    }
+
+    private func createRequest(isLive: Bool) -> VNRecognizeTextRequest {
         let linesCountWithLineLength = [2: [TD2.lineLength, TD3.lineLength], 3: [TD1.lineLength]]
 
         return VNRecognizeTextRequest(completionHandler: { [weak self] request, error in
             DispatchQueue.main.async {
-                guard let self = self, let results = request.results as? [VNRecognizedTextObservation] else { return }
+                guard let self = self else { return }
+                guard error == nil else {
+                    self.delegate?.mrzScanner(self, didReceiveResult: .requestError(error!))
+                    return
+                }
+
+                guard let results = request.results as? [VNRecognizedTextObservation] else { return }
                 guard let possibleLineLenth = linesCountWithLineLength[results.count] else {
                     self.delegate?.mrzScanner(self, didFindBoundingRects: [])
                     self.delegate?.mrzScanner(self, didReceiveResult: .noValidMRZ)
@@ -82,7 +101,7 @@ public class MRZScanner {
                 var boundingRects = [CGRect]()
 
                 for visionResult in results {
-                    guard let line = visionResult.topCandidates(10).map({$0.string}).first(where: {
+                    guard let line = visionResult.topCandidates(10).map({ $0.string }).first(where: {
                         if let firstCode = codes.first {
                             return firstCode.count == $0.count
                         } else {
@@ -105,10 +124,16 @@ public class MRZScanner {
                     return
                 }
 
-                let scanningResult = self.tracker.trackAndGetMostProbableResult(track: result)
-                self.delegate?.mrzScanner(self, didReceiveResult: .success(
-                    mrzResult: scanningResult.mrzResult, accuracy: scanningResult.accuracy)
-                )
+                if isLive {
+                    self.liveResultTracker.track(result: result)
+                    guard let liveScanningResult = self.liveResultTracker.liveScanningResult else {
+                        fatalError("liveScanningResult must be set")
+                    }
+
+                    self.delegate?.mrzScanner(self, didReceiveResult: .liveSuccess(liveScanningResult))
+                } else {
+                    self.delegate?.mrzScanner(self, didReceiveResult: .imageSuccess(MRZResult: result))
+                }
             }
         })
     }

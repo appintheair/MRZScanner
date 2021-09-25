1947c83b0197aa36b45359c8c88b538d1f9c836a
Alexey Salangin <a7ls@ya.ru>
2021-09-25 21:26:33 +0200

    Bump MRZParser version

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/appintheair/MRZParser.git", .upToNextMajor(from: "1.1.1"))
+        .package(url: "https://github.com/appintheair/MRZParser.git", .upToNextMajor(from: "1.1.2"))
     ],
     targets: [
         .target(
